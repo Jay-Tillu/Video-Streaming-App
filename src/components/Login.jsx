1f544dcbfd57efb8db0af824145fdef42eca8ed1
Jay-Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2024-10-17 03:19:27 +0530

    Form Validation Completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
 import bgImage from "../assets/bg-image.png";
+import { checkValidData } from "../utils/form-validation";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const emailReference = useRef(null);
+  const passwordReference = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
+
+  const onSignInButtonClick = () => {
+    const message = checkValidData(
+      emailReference.current.value,
+      passwordReference.current.value
+    );
+
+    setErrorMessage(message);
+  };
+
   return (
-    <div className="h-screen overflow-hidden bg-cover bg-center bg-no-repeat ">
+    <div className="h-screen overflow-hidden bg-cover bg-center bg-no-repeat">
       <Header />
       <div className="absolute">
         <img src={bgImage} alt="Background Image" />
       </div>
-      <form className="w-3/12 absolute p-10 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-65">
+      <form
+        className="w-3/12 absolute p-10 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-65"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -26,16 +43,24 @@ const Login = () => {
           />
         )}
         <input
+          ref={emailReference}
           type="text"
           placeholder="Email address"
           className="px-4 py-3 my-2 w-full rounded-md bg-gray-800 text-[#aeaeae]"
         />
         <input
+          ref={passwordReference}
           type="password"
           placeholder="Password"
           className="px-4 py-3 my-2 w-full rounded-md bg-gray-800  text-[#aeaeae]"
         />
-        <button className="p-2 my-4 bg-[#E50914] w-full rounded-md">
+
+        <p className="text-[#E50914] font-bold p-2 w-full">{errorMessage}</p>
+
+        <button
+          className="p-2 my-4 bg-[#E50914] w-full rounded-md"
+          onClick={onSignInButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p
